688e84139588e46d3f77aeed86dad19ce93f2b0e
Caio Costa <caio.costacarmo@gmail.com>
2019-05-08 22:49:49 -0300

    API Maps

--- estabelecimento_proximo.php
+++ estabelecimento_proximo.php
@@ -1,47 +1,97 @@
+<?php
+
+session_start();
+
+$path_local = $_SERVER['DOCUMENT_ROOT']."/Tcc";
+
+$_SESSION['path_local'] = $path_local;
+
+require_once "$path_local/cms/controller/controllerPessoaJuridica.php";
+
+$controllerPessoaJuridica = new ControllerPessoaJurica();
+
+$rsPessoaJuridica = $controllerPessoaJuridica->listarRegistros();
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
-    <head>
-        <meta charset="utf-8">
-        <title>POP'S - Estabelecimento Próximo</title>
-        <link rel="stylesheet" type="text/css" href="css/style.css">
-        <link rel="stylesheet" type="text/css" href="css/reset.css">
-        <link rel="stylesheet" type="text/css" href="css/estabelecimento_proximo.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <!-- CHAMANDO O JQUERY -->
-        <script src="js/jquery.js">
-        </script>
-        <!-- CHAMANDO O ARQUIVO DE EVENTOS EM JQUERY -->
-        <script src="js/event.js">
-        </script>
-        <script src="js/effects.js"></script>
-    </head>
+<head>
+  <meta charset="utf-8">
+  <title>POP'S - Estabelecimento Próximo</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="css/reset.css">
+  <link rel="stylesheet" type="text/css" href="css/estabelecimento_proximo.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- CHAMANDO O JQUERY -->
+  <script src="js/jquery.js">
+  </script>
+  <!-- CHAMANDO O ARQUIVO DE EVENTOS EM JQUERY -->
+  <script src="js/event.js">
+  </script>
+  <script src="js/effects.js"></script>
 
-    <body>
+  <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDLyCfZiJMTKw8SdqUKqCgYay1rTwnUtF8"></script>
 
-       <header>
-         <?php require_once('header.php')?>
-       </header>
-        <!-- Título da página -->
-        <div class="titulo_pagina font-titulo">
-            <h1>Encontre um estabelecimento próximo a você!</h1>
-        </div>
+  <script type="text/javascript">
 
-        <div class="caixa_central_est_proximo">
-            <form name="frmEstProximo" action="estabelecimento_php" method="POST">
-            <!-- Caixa dos inputs -->
-            <div class="caixa_input_est_proximo">
-                <input class="font-titulo" type="text" name="txtEstProximo" id="txtEstProximo">
-                <input class="font-titulo" type="button" value="Pesquisar">
-            </div>
+  function initialize() {
+    var mapOptions = {
+      center: new google.maps.LatLng(-23.5417691, -46.9098085),
+      zoom: 13,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("caixa_mapa"),
+    mapOptions);
 
-            <!-- caixa do mapa -->
-            <div id="caixa_mapa">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.1030520848626!2d-46.90027078447631!3d-23.52879566627676!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf0154039cb55b%3A0xadf34a919f156950!2sSENAI+Jandira+-+Professor+Vicente+Amato!5e0!3m2!1spt-BR!2sbr!4v1553774027908" width="848" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
-            </div>
-          </form>
-        </div>
+    //nova posição
+    var ponto = new google.maps.LatLng(-23.5417691, -46.9098085);
+    var marker = new google.maps.Marker({
+      position: ponto,//seta posição
+      map: map,//Objeto mapa
+      title:"Hello World!",//string que será exibida quando passar o mouse no marker
+      icon: "img/marcador.png"
+    });
+  }
 
-        <footer> <?php require_once('footer.html')?> </footer>
+  </script>
 
-    </body>
+</head>
+
+<body onload="initialize()">
+
+  <header>
+    <?php require_once('header.php')?>
+  </header>
+  <!-- Título da página -->
+  <div class="titulo_pagina font-titulo">
+    <h1>Encontre um estabelecimento próximo a você!</h1>
+  </div>
+
+  <div class="caixa_central_est_proximo">
+
+    <div id="caixa_enderecos">
+
+      <?php foreach ($rsPessoaJuridica as $pessoaJuridica) { ?>
+
+      <div class="caixa_estabelecimentos">
+        <div class="estabelecimento"><?= $pessoaJuridica->getNomeFantasia() ?></div>
+        <div class="endereco"><?= $pessoaJuridica->getNomeFantasia() ?></div>
+        <div class="telefone"><?= $pessoaJuridica->getNomeFantasia() ?></div>
+      </div>
+
+    <?php } ?>
+
+    </div>
+
+    <!-- caixa do mapa -->
+    <div id="caixa_mapa">
+
+    </div>
+  </form>
+</div>
+
+<footer> <?php require_once('footer.html')?> </footer>
+
+</body>
 </html>
