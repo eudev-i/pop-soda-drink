aa2be2c57ef4500113a41512f8c0a0fcc0b59fff
Murilo Oliveira <murilooliveira99@hotmail.com>
2019-05-18 14:23:49 -0300

    bug consertado

--- js/ws_requests.js
+++ js/ws_requests.js
@@ -4,7 +4,7 @@ var host = window.location.host + "/Tcc";
 $("#btnlogar").click(function(){
     $.ajax({
         type: "POST",
-        url: `http://${host}/pops/backend/services/PJService.php/?op=login`,
+        url: `http://${host}/pops/backend/services/PFService.php/?op=login`,
         dataType: "json",
         data: {"user": $("#txtuser").val(), "senha": $("#txtpassword").val()},
         success: function(data){
@@ -13,10 +13,10 @@ $("#btnlogar").click(function(){
             var html = '';
             if(data.success == true){
                 //redireciona a url p/ dashboard da pj
-                var url = `http://${host}/pops/painel_pessoa_juridica.php`;  
+                var url = `http://${host}/pops/painel_pessoa_fisica.php`;  
 
                 //criando um cookie no js
-                document.cookie = "cnpj="+data.cnpj;  
+                document.cookie = "id_p_fisica="+data.id_p_fisica;  
                 $(location).attr('href',url);
             
             } else {
@@ -27,24 +27,24 @@ $("#btnlogar").click(function(){
     });
 });
 
-//-------ADICIONAR PERFIL------------
-$("#form-perfil").submit(function(evt){
+//Comentário inserido com sucesso!
+$("#form-comentario").submit(function(evt){
+    alert('certo')
     evt.preventDefault();
-    var cnpjCookie = document.cookie;
+    var idPessoaFisicaCookie = document.cookie;
     $.ajax({
         type: "POST",
-        url: `http://${host}/pops/backend/services/PJService.php/?op=addProfile`,
-        data: new FormData($("#form-perfil")[0]),
+        url: `http://${host}/pops/backend/services/PFService.php/?op=addComentario`,
+        data: new FormData($("#form-comentario")[0]),
         cache: false,
         contentType: false,
         processData: false,
         async: true,
         dataType:"json",
         success: function(data){
-            //alert(perfilJuridico.foto);
              var html = "<div>"+data.message+"</div>";
              $("#msg_add").html(html);
-             $("#form-perfil")[0].reset();
+             $("#form-comentario")[0].reset();
         },
         error: function(){
             alert('deu erro');
@@ -52,135 +52,27 @@ $("#form-perfil").submit(function(evt){
     });
 });
 
-//-------ADICIONAR ANUNCIO------------
-$("#form-anuncio").submit(function(evt){
-    evt.preventDefault();
-   
-    $.ajax({
-        type: "POST",
-        url: `http://${host}/pops/backend/services/PJService.php/?op=addAnuncio`,
-        data: new FormData($("#form-anuncio")[0]),
-        cache: false,
-        contentType: false,
-        processData: false,
-        async: true,
-        dataType:"json",
-        success: function(data){
-            //alert(perfilJuridico.foto);
-             var html = "<div>"+data.message+"</div>";
-             $("#msg_add").html(html);
-             $("#form-anuncio")[0].reset();
-             getAdData();
-        },
-        error: function(){
-            alert('deu erro');
-        }
-    });
-});
 
 function getAllData(){
-    var cnpjCookie = document.cookie;
+    var idPessoaFisicaCookie = document.cookie;
     $.ajax({
         type: "POST",
-        url: `http://${host}/pops/backend/services/PJService.php/?op=dashboard`,
+        url: `http://${host}/pops/backend/services/PFService.php/?op=dashboard`,
         dataType: "json",
-        data: {"cnpj":cnpjCookie},
+        data: {"id_p_fisica":idPessoaFisicaCookie},
         success: function(data){
-         
-           
+              
             $('#foto').attr('src', data.foto);
-            $('#nom_fantasia').html(data.nome_fantasia);
-            $('#razao_social').html(data.razao_social);
-            $('#responsavel').html(data.responsavel);
-            $('#user').html(data.usuario);
-            $('#cnpj').html(data.cnpj);
-            $('#email').html(data.email);
+            $('#nome').html(data.nome);
+            $('#cpf').html(data.cpf);
+            $('#celular').html(data.celular);
             $('#logradouro').html(data.logradouro);
+            $('#num').html(data.num);
             $('#bairro').html(data.bairro);
-            $('#tel').html(data.telefone);
-            $('#num').html(data.numero);
-            $('#cep').html(data.cep);
+            $('#cidade').html(data.cidade);
             $('#uf').html(data.uf);
-
-            //preenchendo campos da modal
-            $('#txt_nome').val(data.razao_social);
-            $('#txt_logradouro').val(data.logradouro);
-            $('#txt_num').val(data.numero);
-            $('#txt_cidade').val(data.cidade);
-            $('#txt_uf').val(data.uf);
            
         }
     });
 }
-
-function getPerfilData(){
-    var cnpjCookie = document.cookie;
-    $.ajax({
-        type: "POST",
-        url: `http://${host}/pops/backend/services/PJService.php/?op=perfis`,
-        dataType: "json",
-        data: {"cnpj":cnpjCookie},
-        success: function(data){
-            
-            var html = '<div class="caixa_users_add">';
-            
-            
-            for(var i = 0; i < data.length; i++){
-               console.log('entrou no for');
-               html+= '<p class="font-negrito font-texto">Users: </p><p class="font-texto nome_perfil" >'+data[i].usuario+'</p>';
-               html+= `<img class="elemento_direita img_perfil" src='http://${host}/pops/img/temp/${data[i].foto}' width="30" height="30" alt="ola" title="ola"></img>`;
-               html+= `<div class="edit">
-                            <img class="elemento_direita img_perfil edit"  src='http://${host}/cms/view/img/editar.png' width="30" height="30" alt="ola" title="ola">
-                        </img></div><br>`;
-           }
-           html+='</div>';
-           $("#container_perfil").html(html);
-           //alert(html);
-           
-        }
-    });
-}
-
-function getAdData(){
-   
-    var cnpjCookie = document.cookie;
-    $.ajax({
-        type: "POST",
-        url: `http://${host}/pops/backend/services/PJService.php/?op=ads`,
-        dataType: 'json',
-        data: {"cnpj":cnpjCookie},
-        success: function(data){
-          var html = '<div class="container_card_anuncio">';
-          for(var i = 0; i < data.length; i++){
-            
-               html+= `<a class="click_me" href="#" onclick="callModal('modal-anuncio.php');$('#container').fadeIn(600);"><div class="card_anuncio">`;
-               html+= `<div class="card_img_anuncio"><img class="img_anuncio" src='http://${host}/cms/view/img/temp/${data[i].foto}'  alt="ads" title="ads"></img></div>`;
-               html+= `<div class="options">Descrição</div>`;
-               html+= `<div class="desc_anuncio">${data[i].descricao}</div>`;
-            
-               html+= `</div></a>`;
-
-            
-           }
-          $("#container_card_anuncio").html(html);
-        },
-        error: function(xhr){
-            var errorMessage = xhr.status + ': ' + xhr.statusText
-            alert('Error - ' + errorMessage);
-           
-        }
-    });
-}
-
-function callModalWithData(data){
-    $.ajax({
-      type: "GET",
-      url: 'modal-anuncio.php',
-      success: function(dados){
-        $("#modal").html(dados)
-      }
-    });
-  }
-
-
 
